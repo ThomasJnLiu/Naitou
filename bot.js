aa9a67c005e4535f21c6eb861297a7fab35a561e
Thomas <thomasjnliu@gmail.com>
2019-04-10 22:57:26 -0400

    add command processing

--- bot.js
+++ bot.js
@@ -3,8 +3,23 @@ const client = new Discord.Client()
 
 client.on('ready', () => {
     // Set bot status to: "Playing with JavaScript"
-    client.user.setActivity("Itou", { type: "WATCHING" })
+    client.user.setActivity("Itou", { type: "WATCHING" });
 })
+
+client.on('message', (receivedMessage) => {
+    if (receivedMessage.author == client.user) { // Prevent bot from responding to its own messages
+        return
+    }
+
+    if (receivedMessage.content.startsWith("!")) {
+        processCommand(receivedMessage);
+    }
+})
+
+function processCommand(receivedMessage) {
+    return;
+}
+
 
 const { token } = require('./config.json');
 client.login(token);
